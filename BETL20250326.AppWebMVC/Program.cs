0108ddb530e8e64790cfe19c40d5627f50ffe827
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-03-26 10:27:27 -0600

    se agregaron las cookies a program

--- BETL20250326.AppWebMVC/Program.cs
+++ BETL20250326.AppWebMVC/Program.cs
@@ -1,4 +1,5 @@
 using BETL20250326.AppWebMVC.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,14 @@ builder.Services.AddDbContext<Test20250326DbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
 });
-
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie((o) =>
+{
+    o.LoginPath = new PathString("/Usuarios/login");
+    o.AccessDeniedPath = new PathString("/Usuarios/login");
+    o.ExpireTimeSpan = TimeSpan.FromHours(8);
+    o.SlidingExpiration = true;
+    o.Cookie.HttpOnly = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
