9bc675d570531b9443289bdb0be539bbf81baa69
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-03-26 10:35:00 -0600

    se agrego autorice

--- BETL20250326.AppWebMVC/Controllers/UsersController.cs
+++ BETL20250326.AppWebMVC/Controllers/UsersController.cs
@@ -15,6 +15,7 @@ using System.Security.Claims;
 
 namespace BETL20250326.AppWebMVC.Controllers
 {
+    [Authorize(Roles = "ADMINISTRADOR")]
     public class UsersController : Controller
     {
         private readonly Test20250326DbContext _context;
