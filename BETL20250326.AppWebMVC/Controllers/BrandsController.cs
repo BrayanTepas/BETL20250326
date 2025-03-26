97809213265c20c721417ca9829dbd0ea78fef43
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-03-26 08:17:37 -0600

    se creo buscar, diseño y paginacion de index brands

--- BETL20250326.AppWebMVC/Controllers/BrandsController.cs
+++ BETL20250326.AppWebMVC/Controllers/BrandsController.cs
@@ -19,9 +19,17 @@ namespace BETL20250326.AppWebMVC.Controllers
         }
 
         // GET: Brands
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(Brand marca, int topRegistro = 10)
         {
-            return View(await _context.Brands.ToListAsync());
+            var query = _context.Brands.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(marca.BrandName))
+                query = query.Where(s => s.BrandName.Contains(marca.BrandName));
+
+            if (topRegistro > 0)
+                query = query.Take(topRegistro);
+
+
+            return View(await query.ToListAsync());
         }
 
         // GET: Brands/Details/5
